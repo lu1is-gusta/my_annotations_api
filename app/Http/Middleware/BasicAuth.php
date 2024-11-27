337dbe584b4dc906592e1b1140c412a0000bd6f3
lu1is.gusta <gustavovacodar@gmail.com>
2024-11-27 15:34:53 -0300

    creation of middleware for basic authentication with the API

---
+++ app/Http/Middleware/BasicAuth.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class BasicAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            $serverAuthUser = $request->server('PHP_AUTH_USER');
+            $serverAuthPassword = $request->server('PHP_AUTH_PW');
+            $localAuthUser = env('AUTH_BASIC_USER');
+            $localAuthPassword = env('AUTH_BASIC_PASSWORD');
+            
+            if($serverAuthUser === $localAuthUser && $serverAuthPassword === $localAuthPassword){
+                return $next($request);
+            }
+
+            return new Response('Unauthorized', 401, ['WWW-Authenticate' => 'Basic']);
+
+        } catch(Exception $error){
+
+            return $error;
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,13 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Sanctum\Sanctum;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\NoteController;
+use App\Http\Middleware\BasicAuth;
 
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::resources([
-        'users' => UserController::class,
-        'notes' => NoteController::class
-    ]);
+Route::middleware([BasicAuth::class])->group(function () { //juntar com o middleware de baixo
+    Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+        Route::resources([
+            'users' => UserController::class,
+            'notes' => NoteController::class
+        ]);
+    });
 });
